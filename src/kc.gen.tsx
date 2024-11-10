14f89ac49c280dd3e4833e0d9022fd4932b09022
CJ Virtucio <cjv287@gmail.com>
2024-11-10 08:16:19 -0500

    showing toph background image

--- src/kc.gen.tsx
+++ src/kc.gen.tsx
@@ -10,9 +10,9 @@
 
 import { lazy, Suspense, type ReactNode } from "react";
 
-export type ThemeName = "keycloakify-starter";
+export type ThemeName = "cjvautomation-keycloakified";
 
-export const themeNames: ThemeName[] = ["keycloakify-starter"];
+export const themeNames: ThemeName[] = ["cjvautomation-keycloakified"];
 
 export type KcEnvName = never;
 
