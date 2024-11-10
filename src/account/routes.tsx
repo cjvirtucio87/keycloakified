a8a34c5b01b25a840066d0186e6318f99e5be2f2
CJ Virtucio <cjv287@gmail.com>
2024-11-10 16:18:58 -0500

    more fixes

--- src/account/routes.tsx
+++ src/account/routes.tsx
@@ -1,10 +1,16 @@
 import { environment } from "@keycloakify/keycloak-account-ui/environment";
 import { Root } from "@keycloakify/keycloak-account-ui/root/Root";
 import { ErrorPage } from "@keycloakify/keycloak-account-ui/root/ErrorPage";
-import { RouteObject, PersonalInfoRoute, DeviceActivityRoute, LinkedAccountsRoute, SigningInRoute, GroupsRoute, OrganizationsRoute, PersonalInfoRoute, ResourcesRoute, ContentRoute, Oid4VciRoute } from "@keycloakify/keycloak-account-ui/routes";
+import type { RouteObject } from "react-router-dom";
+import { PersonalInfoRoute, DeviceActivityRoute, LinkedAccountsRoute, SigningInRoute, GroupsRoute, OrganizationsRoute, ResourcesRoute, ContentRoute, Oid4VciRoute } from "@keycloakify/keycloak-account-ui/routes";
 
 import { Applications } from './Applications'
 
+
+export const ApplicationsRoute: RouteObject = {
+  path: "applications",
+  element: <Applications />,
+};
 
 export const RootRoute: RouteObject = {
   path: decodeURIComponent(new URL(environment.baseUrl).pathname),
